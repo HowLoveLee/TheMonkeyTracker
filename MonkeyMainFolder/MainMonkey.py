7d6659062975a4496a8f7970a4b9179768d0b9ce
Aleksandr Soloshenko <capcom2me@gmail.com>
2023-10-06 23:38:49 +0700

    Fixed: don\t use absolute paths

--- MonkeyMainFolder/MainMonkey.py
+++ MonkeyMainFolder/MainMonkey.py
@@ -2,17 +2,21 @@ import sys
 from PyQt6.QtWidgets import QApplication
 
 from MonkeyMainFolder.MonkeyApplication import MyWindow
+from MonkeyMainFolder.Settings import ROOT_PATH
 
-def loadStyleSheet(filename):
+
+def loadStyleSheet(filename: str):
     with open(filename, "r") as f:
         return f.read()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    tyle = loadStyleSheet("C:\Dev\PythonProjects\TheMonkeyTracker\Themes\IntellijThemeGrey.qss")
-    #app.setStyleSheet(tyle)
+    tyle = loadStyleSheet(
+        ROOT_PATH.parent.joinpath("Themes/IntellijThemeGrey.qss").as_posix()
+    )
+    # app.setStyleSheet(tyle)
     window = MyWindow()
     window.show()
     sys.exit(app.exec())
